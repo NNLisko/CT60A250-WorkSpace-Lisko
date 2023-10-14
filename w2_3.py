b26af103a2f490b3b07cafd469cc8a786641dc2b
NNLisko <141261641+NNLisko@users.noreply.github.com>
2023-10-14 21:04:01 +0300

    Update w2_3.py

--- w2_3.py
+++ w2_3.py
@@ -1,5 +1,7 @@
+#takes word as an input
 word = input('Enter a long word:\n')
 
+#prints different indexings of the word
 print(f'The first five letters are: {word[:5]}')
 print(f'The last five letters are: {word[-5:]}')
 print(f'Letters 2, 3, 4 and 5 are: {word[1:5]}')
