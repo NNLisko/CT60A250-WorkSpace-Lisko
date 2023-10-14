bf9f4810db0ea70c486a0dc3d65467a92e8f5f08
NNLisko <141261641+NNLisko@users.noreply.github.com>
2023-10-14 21:00:09 +0300

    w1_4.py

--- w1_4.py
+++ w1_4.py
@@ -1,5 +1,8 @@
+#takes input
 temp = input('Enter the temperature in celcius: ')
 
+#converts integer input based on celcius-fahrenheit conversion
 tempF = round((float(temp) * (9/5)) + 32, 1)
 
+#prints result
 print(f'{temp} degree celcius is equal to {tempF} degree farhenheit')
