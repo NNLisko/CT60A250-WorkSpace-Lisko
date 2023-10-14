c66b5aac8ecacb34cc92bab0178c0a5fd49aa0f3
NNLisko <141261641+NNLisko@users.noreply.github.com>
2023-10-14 20:58:53 +0300

    Update w1_3.py

--- w1_3.py
+++ w1_3.py
@@ -1,5 +1,8 @@
+#Takes two variables
 n1 = input('Enter the first number: ')
 n2 = input('Enter the second number: ')
+
+#creates a variable that sums the two inputs
 summ = int(n1) + int(n2)
 
 print(f'Sum: {summ}')
