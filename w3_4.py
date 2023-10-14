f5179e1c56a2689ab08a68f9ee8d78704947d09e
NNLisko <141261641+NNLisko@users.noreply.github.com>
2023-10-14 21:12:42 +0300

    Update w3_4.py

--- w3_4.py
+++ w3_4.py
@@ -1,6 +1,8 @@
+#takes two strings as input
 w1 = input('Enter word 1:\n')
 w2 = input('Enter word 2:\n')
 
+#checks which word comes earlier in order
 if w1 < w2:
     print(f"'{w1}' comes earlier in order than '{w2}'.")
 elif w2 < w1:
@@ -8,6 +10,7 @@ elif w2 < w1:
 else:
     print('The words are the same.')
 
+#checks if letter z is found within word
 if 'z' in w1 and 'z' in w2:
     print(f"Letter 'z' is found in the word '{w1}'")
     print(f"Letter 'z' is found in word '{w2}'")
@@ -19,8 +22,10 @@ else:
     if 'z' in w2:
         print(f"Letter 'z' is found in word '{w2}'.")
 
+#takes third string as input
 w3 = input('Enter a word to be tested:\n')
 
+#checks if w3 is palindrome
 if w3 == w3[::-1]:
     print(f"'{w3}' is a palindrome.")
 else:
