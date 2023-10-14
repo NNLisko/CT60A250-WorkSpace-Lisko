141b4fa6e2b114f743566d8ac473f31985191396
NNLisko <141261641+NNLisko@users.noreply.github.com>
2023-10-14 21:05:36 +0300

    Update w2_4.py

--- w2_4.py
+++ w2_4.py
@@ -1,5 +1,11 @@
+#takes a word as an input
 word = input('Give a word:\n')
+
+#prints
 print(f'The length of the word is {len(word)}')
 
+#takes int as an input
 n1 = int(input(f'Give an integer smaller than or equal to {len(word)}:\n'))
+
+#prints the word and replaces nth character with *
 print(word[:n1 - 1] + '*' + word[n1:])
