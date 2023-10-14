305c338d7969f114a4f8fb4318d7db2e51cacd53
NNLisko <141261641+NNLisko@users.noreply.github.com>
2023-10-14 21:01:29 +0300

    Update w1_5.py

--- w1_5.py
+++ w1_5.py
@@ -1,8 +1,13 @@
+#takes two integers as input
 n1 = input('Give the first integer: ')
 n2 = input('Give the first integer: ')
 
+#divides input integers
 div = round(int(n1) // int(n2),1)
+
+#calculates and rounds the remainder
 rmd = round(int(n1) % int(n2),1)
 
+#prints
 print(f'The number {n2} Goes to number {n1} {div} times')
 print(f'The reminder is {rmd}')
