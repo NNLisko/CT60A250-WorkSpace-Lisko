7289a6edd2e24944a682e0635148f15e083a4d50
NNLisko <141261641+NNLisko@users.noreply.github.com>
2023-10-14 21:02:28 +0300

    Update w2_1.py

--- w2_1.py
+++ w2_1.py
@@ -1,5 +1,10 @@
+#takes name, integer and float as input
 name = input('Enter your name:\n')
 n1 = int(input('Enter an integer:\n'))
 n2 = float(input('Enter a float:\n'))
+
+#exponents the float with the integer
 exp = round(n2 ** n1,2)
+
+#prints
 print(f'Decimal {n2} to power {n1} is {exp}\nThank you for using the program {name}!')
