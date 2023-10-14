e091cce075aad991fc9c6a996b2e89334ed1e068
NNLisko <141261641+NNLisko@users.noreply.github.com>
2023-10-14 21:03:12 +0300

    Update w2_2.py

--- w2_2.py
+++ w2_2.py
@@ -1,5 +1,7 @@
+#takes first and last name as input
 lName = input('Please enter your lastname:\n')
 fName = input('Please enter your firstname:\n')
 
+#prints using sep and end methods
 print('Your email address is: ' + fName,lName, sep='.', end='@')
 print('lut.fi')
