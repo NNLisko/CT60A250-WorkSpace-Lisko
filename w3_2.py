549d281f085f7915067e4445014ee3acdbe6378f
NNLisko <141261641+NNLisko@users.noreply.github.com>
2023-10-14 21:09:28 +0300

    Update w3_2.py

--- w3_2.py
+++ w3_2.py
@@ -1,8 +1,11 @@
+#yes/no input
 yesno = input('Do you want to stop the execution of the program (y/Y):\n')
 
+#if user wants to end program, program exits with bye
 if yesno == 'Y' or yesno == 'y':
     print('Bye!')
 
+#if not, program checks for username and password
 elif yesno == 'n' or yesno == 'N':
     usr = input('Enter username:\n')
     pswrd = input('Enter password:\n')
