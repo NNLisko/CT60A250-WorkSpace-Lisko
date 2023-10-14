c98918eab7c47643e6fc0ee4af911d40ac18b4ce
NNLisko <141261641+NNLisko@users.noreply.github.com>
2023-10-14 21:07:21 +0300

    Update w3_1.py

--- w3_1.py
+++ w3_1.py
@@ -1,5 +1,7 @@
+#takes a year as an int input
 y1 = int(input('Enter a year:\n'))
 
+#if-elif-else to determine if the year is a leap year
 if y1 % 100 == 0 and not y1 % 400 == 0:
     print(f'{y1} is not a leap year.')
 elif y1 % 4 == 0:
